066ca7faefcd0c1f557d747f3a5e85c59a4d5b82
Calum McConnell <calumlikesapplepie@gmail.com>
2021-03-09 17:31:33 -0800

    Get a basic outline going for Autonomus Trajectory

---
+++ src/main/java/frc/robot/commands/AutonomousTrajectoryRioCommand.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.controller.RamseteController;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import frc.robot.Robot;
+import frc.robot.subsystems.BasicKinematicsController;
+
+/** This form of the class runs all code on the RoboRIO*/
+public class AutonomousTrajectoryRioCommand extends RamseteCommandWpilib {
+    BasicKinematicsController navigation;
+
+    AutonomousTrajectoryRioCommand(Trajectory trajectory, BasicKinematicsController navigatorSubsystem){
+        super(
+            trajectory, 
+            () -> {return navigatorSubsystem.getPosition();}, // Lambda supplies pose for robot
+            navigatorSubsystem.getRamseteController(), // Grab kinematics controller from Robot.java
+            navigatorSubsystem.getFeedforward(), 
+            navigatorSubsystem.getKinematics(), 
+            () -> {return navigatorSubsystem.getWheelSpeeds();}, 
+            navigatorSubsystem.getLeftPidController(), 
+            navigatorSubsystem.getRightPidController(), 
+            (Double leftVoltage, Double rightVoltage) -> {  // yes, I DO mean the object type.
+                System.out.println("hi");
+            }
+        );
+        navigation = navigatorSubsystem;
+
+    }
+    
+    public void execute(){
+        navigation.updateOdometer();
+        super.execute();
+    }
+}
