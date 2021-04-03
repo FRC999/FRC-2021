8def98d51a07de7f3abe45b057857ab1df34ad43
JEWertz <john.wertz@yale.edu>
2021-04-03 02:50:07 -0400

    Added Barrel Run auto command group

--- src/main/java/frc/robot/commands/AutoBarrelRunCommandGroup.java
+++ src/main/java/frc/robot/commands/AutoBarrelRunCommandGroup.java
@@ -10,66 +10,63 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.Robot;
 
-public class AutoBouncePathCommandGroup extends CommandGroup {
+public class AutoBarrelRunCommandGroup extends CommandGroup {
   private static double targetAngle = 0;
 
   /**
    * Add your docs here.
    */
-  public AutoBouncePathCommandGroup() {
+  public AutoBarrelRunCommandGroup() {
 
     //DriveForwardCommand arguments are in inches
     //DriveTurnCommand wants arguments in relative angle cw = pos, ccw = neg
-    targetAngle = -62;
+    targetAngle = 26;
     addSequential(new DriveTurnCommand(targetAngle));
-    addSequential(new DriveForwardCommand(65));
+    addSequential(new DriveForwardCommand(178));
     //targetAngle = (current heading - desired heading)*-1
-    targetAngle = (Robot.navXSubsystem.getYaw()-(-118))*-1;
+    targetAngle = (Robot.navXSubsystem.getYaw()-(7))*-1;
     addSequential(new DriveTurnCommand(targetAngle));
-    addSequential(new DriveForwardCommand(-106));
-    targetAngle = (Robot.navXSubsystem.getYaw()-(-76))*-1;
+    addSequential(new DriveForwardCommand(-111));
+    targetAngle = (Robot.navXSubsystem.getYaw()-(-18))*-1;
     addSequential(new DriveTurnCommand(targetAngle));
-    addSequential(new DriveForwardCommand(95));
-    targetAngle = (Robot.navXSubsystem.getYaw()-(-103))*-1;
+    addSequential(new DriveForwardCommand(215));
+    targetAngle = (Robot.navXSubsystem.getYaw()-(13))*-1;
     addSequential(new DriveTurnCommand(targetAngle));
-    addSequential(new DriveForwardCommand(-92));
-    targetAngle = (Robot.navXSubsystem.getYaw()-(0))*-1;
+    addSequential(new DriveForwardCommand(-99));
+    targetAngle = (Robot.navXSubsystem.getYaw()-(47))*-1;
     addSequential(new DriveTurnCommand(targetAngle));
-    addSequential(new DriveForwardCommand(52));
-    targetAngle = (Robot.navXSubsystem.getYaw()-(-90))*-1;
+    addSequential(new DriveForwardCommand(155));
+    targetAngle = (Robot.navXSubsystem.getYaw()-(118))*-1;
     addSequential(new DriveTurnCommand(targetAngle));
-    addSequential(new DriveForwardCommand(92));
-    targetAngle = (Robot.navXSubsystem.getYaw()-(-123))*-1;
+    addSequential(new DriveForwardCommand(-52));
+    targetAngle = (Robot.navXSubsystem.getYaw()-(178))*-1;
     addSequential(new DriveTurnCommand(targetAngle));
-    addSequential(new DriveForwardCommand(-65));
-    targetAngle = (Robot.navXSubsystem.getYaw()-(-180))*-1;
-    addSequential(new DriveTurnCommand(targetAngle));
-    addSequential(new DriveForwardCommand(-12));
+    addSequential(new DriveForwardCommand(310));
     addSequential(new DriveStopCommand());
 
      //***Relative turning based on encoder readings ..No heading corrections */
     //DriveForwardCommand arguments are in inches
     //DriveTurnCommand wants arguments in relative angle cw = pos, ccw = neg
 /** 
-    addSequential(new DriveTurnCommand(-62));
+    addSequential(new DriveTurnCommand(26));
     addSequential(new DriveForwardCommand(65));
-    addSequential(new DriveTurnCommand(-55));
-    addSequential(new DriveForwardCommand(-106));
-    addSequential(new DriveTurnCommand(42));
-    addSequential(new DriveForwardCommand(95));
-    addSequential(new DriveTurnCommand(-27));
-    addSequential(new DriveForwardCommand(-92));
-    addSequential(new DriveTurnCommand(84));
-    addSequential(new DriveForwardCommand(52));
-    addSequential(new DriveTurnCommand(-90));
-    addSequential(new DriveForwardCommand(92));
-    addSequential(new DriveTurnCommand(-35));
-    addSequential(new DriveForwardCommand(-65));
-    addSequential(new DriveTurnCommand(-55));
-    addSequential(new DriveForwardCommand(-12));
+    addSequential(new DriveTurnCommand(-19));
+    addSequential(new DriveForwardCommand(-111));
+    addSequential(new DriveTurnCommand(-25));
+    addSequential(new DriveForwardCommand(215));
+    addSequential(new DriveTurnCommand(31));
+    addSequential(new DriveForwardCommand(-99));
+    addSequential(new DriveTurnCommand(34));
+    addSequential(new DriveForwardCommand(155));
+    addSequential(new DriveTurnCommand(71));
+    addSequential(new DriveForwardCommand(-52));
+    addSequential(new DriveTurnCommand(60));
+    addSequential(new DriveForwardCommand(310));
     addSequential(new DriveStopCommand());
 */
    
+
+
 
     // Add Commands here:
     // e.g. addSequential(new Command1());
